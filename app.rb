ef0ea82730f9e27eff643941fe5614fcf674b663
Ryan Baumann <ryan.baumann@gmail.com>
2013-08-15 14:37:05 -0400

    Better malformed request handling

--- app.rb
+++ app.rb
@@ -15,18 +15,27 @@ options '/:object_id' do
 end
 
 get '/:object_id' do
-  xml=`git --git-dir=/Users/ryan/source/dc3/idp.data/.git --work-tree=/Users/ryan/source/dc3/idp.data show #{params[:object_id]}`
-  edition=Nokogiri::XML(xml).xpath("//tei:div[@type='edition']",'tei'=>'http://www.tei-c.org/ns/1.0').first.to_s
-  uri = URI.parse('http://localhost:9999/')
-  http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Post.new('/')
-  request.set_form_data({'type' => 'epidoc', 'direction' => 'xml2nonxml', 'content' => edition})
+  if params[:object_id] =~ /^[a-fA-F0-9]{40}$/
+    exists = `git --git-dir=/Users/ryan/source/dc3/idp.data/.git --work-tree=/Users/ryan/source/dc3/idp.data cat-file -e #{params[:object_id]}`
+    if $?.to_i == 0
+      xml = `git --git-dir=/Users/ryan/source/dc3/idp.data/.git --work-tree=/Users/ryan/source/dc3/idp.data show #{params[:object_id]}`
+      edition=Nokogiri::XML(xml).xpath("//tei:div[@type='edition']",'tei'=>'http://www.tei-c.org/ns/1.0').first.to_s
+      uri = URI.parse('http://localhost:9999/')
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new('/')
+      request.set_form_data({'type' => 'epidoc', 'direction' => 'xml2nonxml', 'content' => edition})
 
-  cache_control :public, :max_age => 31536000
-  response.headers["Vary"] = "Accept-Encoding"
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  response.headers["X-XSS-Protection"] = "0"
-  response.headers["Content-Type"] = "application/json"
+      cache_control :public, :max_age => 31536000
+      response.headers["Vary"] = "Accept-Encoding"
+      response.headers["Access-Control-Allow-Origin"] = "*"
+      response.headers["X-XSS-Protection"] = "0"
+      response.headers["Content-Type"] = "application/json"
 
-  return http.request(request).body
+      return http.request(request).body
+    else
+      halt 404
+    end
+  else
+    halt 400
+  end
 end
